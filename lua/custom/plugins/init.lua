1ee531a8dcd1a1c696e9b8441b7548dabcfcc967
GeorgiyKuzora <rafale87@gmail.com>
2023-08-27 21:50:57 +0300

    Moved config data in separate file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -137,10 +137,4 @@ return {
     },
     build = ':TSUpdate',
   },
-
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
 }
